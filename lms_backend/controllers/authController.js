0daae5d5b559995d59fcb094c560146e8af68d8a
ADHOC123-cmd <hr@adhocnetwork.tech>
2026-07-13 15:19:38 +0530

    Change error message for unregistered email

--- lms_backend/controllers/authController.js
+++ lms_backend/controllers/authController.js
@@ -146,7 +146,7 @@ exports.login = async (req, res) => {
     if (!user) {
       return res.status(401).json({
         success: false,
-        message: 'Invalid credentials',
+        message: 'Email was not registered',
       });
     }
 
